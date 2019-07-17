bbdc158cde40fa4e5ade314e55212be3733eca9c
Vladislav Simanin <Bayron013@gmail.com>
2019-07-17 19:09:59 +0300

    Добавил сравнения множественных значений в тесты удаления и модификации групп.

--- ab-web-tests/src/test/java/ru/avtotest/ab/tests/GroupUpdateTests.java
+++ ab-web-tests/src/test/java/ru/avtotest/ab/tests/GroupUpdateTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.avtotest.ab.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupUpdateTests extends TestBase{
@@ -15,13 +16,21 @@ public class GroupUpdateTests extends TestBase{
       app.getGroupHelper().createGroup(new GroupData("Group 1", "Group header", "Group footer"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(before.size() - 2);
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().editGroup();
-    app.getGroupHelper().fillGroupForm(new GroupData("Group 8", "Група крови", "На рукаве"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "Group 8", "Група крови", "На рукаве");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().updateGroup();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(before.size(), after.size());
+
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
     app.getSessionHelper().logout();
+
   }
 }
