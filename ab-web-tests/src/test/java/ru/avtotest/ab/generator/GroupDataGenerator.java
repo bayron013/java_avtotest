f3dbdebfe00edf5b2d4baad32a83d42fda6c7e30
Vladislav Simanin <Bayron013@gmail.com>
2019-08-01 11:07:12 +0300

    Модифицировал тест по созданию групп, чтобы он читал файлы формата xml

--- ab-web-tests/src/test/java/ru/avtotest/ab/generator/GroupDataGenerator.java
+++ ab-web-tests/src/test/java/ru/avtotest/ab/generator/GroupDataGenerator.java
@@ -4,8 +4,10 @@ package ru.avtotest.ab.generator;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.avtotest.ab.model.GroupData;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -21,6 +23,9 @@ public class GroupDataGenerator {
   @Parameter(names = "-f", description = "Target file")
   public String file;
 
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
   public static void main (String[] args) throws IOException {
     GroupDataGenerator generator = new GroupDataGenerator();
     JCommander jComander = new JCommander(generator);
@@ -35,10 +40,25 @@ public class GroupDataGenerator {
 
   private void run() throws IOException {
     List<GroupData> groups = generateGroups(count);
-    save(groups, new File(file));
+    if (format.equals("csv")) {
+      saveAsCsv(groups, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(groups, new File(file));
+    } else  {
+      System.out.println("Unrecignized format " + format);
+    }
   }
 
-  private void save(List<GroupData> groups, File file) throws IOException {
+  private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+    XStream xStream = new XStream();
+    xStream.processAnnotations(GroupData.class);
+    String xml = xStream.toXML(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
       writer.write(String.format("%s;%s;%s\n", group.getName(),
--- ab-web-tests/src/test/java/ru/avtotest/ab/model/GroupData.java
+++ ab-web-tests/src/test/java/ru/avtotest/ab/model/GroupData.java
@@ -1,9 +1,14 @@
 package ru.avtotest.ab.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 import java.util.Objects;
 
+@XStreamAlias("group")
 public class GroupData {
+  @XStreamOmitField
   private int id = Integer.MAX_VALUE;
   private String name;
   private String header;
