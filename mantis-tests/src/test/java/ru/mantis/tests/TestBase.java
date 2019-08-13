6528e5db9cc93853e5a3f05fdb45e8d81706f338
bayron013 <bayron013@gmail.com>
2019-08-13 23:12:06 +0400

    Добавлен новый модуль для тестов для Mantis

---
+++ mantis-tests/src/test/java/ru/mantis/tests/TestBase.java
@@ -0,0 +1,24 @@
+package ru.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.mantis.appmanger.ApplcationManager;
+
+public class TestBase {
+
+  protected static final ApplcationManager app
+          = new ApplcationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+
+
+}
