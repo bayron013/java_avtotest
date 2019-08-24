46b90db938dab30bdbcbd4d0ae7b8b54567eece4
bayron013 <bayron013@gmail.com>
2019-08-24 19:58:01 +0400

    Добавил тест с использование програмного интерфейса Github

---
+++ github-sample/src/test/java/github/GithubTests.java
@@ -0,0 +1,20 @@
+package github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub(" 005e54690a68aac797ed00168c1a54d178be30b4");
+    RepoCommits commits = github.repos().get(new Coordinates
+            .Simple("bayron013", "java_avtotest")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
