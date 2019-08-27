b0cd934d8c38e7acbb347d483e0a44fdd23fb9ab
bayron013 <bayron013@gmail.com>
2019-08-27 04:22:09 +0400

    Добавлена реализация тестов с баг трекеров bugify

--- resr-sample/src/test/java/rest/RestTests.java
+++ resr-sample/src/test/java/rest/RestTests.java
@@ -1,12 +1,5 @@
 package rest;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.message.BasicNameValuePair;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -14,7 +7,7 @@ import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestTests extends TestBase{
 
   @Test
   public void testCreateIssue() throws IOException {
@@ -26,24 +19,13 @@ public class RestTests {
     assertEquals(newIssues, oldIssues);
   }
 
-  private Set<Issue> getIssues() throws IOException {
-    String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues.json"))
-            .returnContent().asString();
-    JsonElement parsed = new JsonParser().parse(json);
-    JsonElement issues = parsed.getAsJsonObject().get("issues");
-    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
-  }
-
-  private Executor getExecutor() {
-    return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
-  }
-
-  private int createIssue(Issue newIssue) throws IOException {
-    String json = getExecutor().execute(Request.Post("http://bugify.stqa.ru/api/issues.json")
-            .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
-                    new BasicNameValuePair("discription", newIssue.getDescription())))
-            .returnContent().asString();
-    JsonElement parsed = new JsonParser().parse(json);
-    return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  @Test
+  public void testSkipIfNotFixedIssuesExist() throws IOException {
+    Set<Issue> issues = getIssues();
+    for (Issue issue : issues) {
+      System.out.println("Issue id: " + issue.getId());
+      System.out.println("Issue status: "+ issue.getState());
+      skipIfNotFixed(issue.getId());
+    }
   }
 }
